1045ebcec2ada9c323722a2467ad71c36fbfdc91
Gerardo Cruz Maldonado <gerardo7vcruz@gmail.com>
2025-06-01 18:21:45 -0700

    Modificacion Vite y packages para github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: '/3DPortafolio/', // 👈 nombre exacto del repo
   plugins: [
     react(),
     tailwindcss(),
